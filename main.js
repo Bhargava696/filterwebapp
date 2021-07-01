68d344d223232bc6a5dc11049102de5918b1c859
Bhargava696 <74226314+Bhargava696@users.noreply.github.com>
2021-07-01 16:53:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,39 @@
-function preload(){}
-function setup(){
-    abc = createCanvas(400,300);
+pos_x = 0;
+pos_y = 0;
+
+function preload() {
+    images = loadImage("https://i.postimg.cc/TP6gQD17/Moustache-Graphic.png");
+}
+
+function setup() {
+    abc = createCanvas(400, 300);
     abc.center();
     dions = createCapture(VIDEO);
-    dions.size(400,300);
+    dions.size(400, 300);
     dions.hide();
-    dan = ml5.poseNet(dions,modelLoaded);
-    dan.on("pose",gotPoses);
+    dan = ml5.poseNet(dions, modelLoaded);
+    dan.on("pose", gotPoses);
 }
-function draw(){
-    image(dions,0,0,400,300);
+
+function draw() {
+    image(dions, 0, 0, 400, 300);
+    image(images, pos_x, pos_y, 100, 50);
 }
-function capt(){
+
+function capt() {
     save("image.png");
 }
-function modelLoaded(){
+
+function modelLoaded() {
     console.log("Model is loaded")
 }
-function gotPoses(results){
-    if(results.length > 0){
+
+function gotPoses(results) {
+    if (results.length > 0) {
         console.log(results);
-        console.log("x = "+results[0].pose.nose.x);
-        console.log("y = "+results[0].pose.nose.y);
+        pos_x = results[0].pose.nose.x - 50;
+        pos_y = results[0].pose.nose.y - 2;
+        console.log("x = " + pos_x);
+        console.log("y = " + pos_y);
     }
 }
